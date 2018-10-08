8d453b7d6f13bb3c2dc1df3e02a306aa0b7a7a61
Tejaswi789 <42660107+Tejaswi789@users.noreply.github.com>
2018-10-08 15:33:42 -0500

    commit
    commit

--- ICP_7/Source/src/app/app.firebase.config.ts
+++
@@ -1,9 +0,0 @@
- export const FIREBASE_CONFIG= {
-    apiKey: "AIzaSyB41hu9diWn3A-Yk0_gF9zWmFwujuIQvKg",
-    authDomain: "icp7-d4bd5.firebaseapp.com",
-    databaseURL: "https://icp7-d4bd5.firebaseio.com",
-    projectId: "icp7-d4bd5",
-    storageBucket: "icp7-d4bd5.appspot.com",
-    messagingSenderId: "464905549599"
-    }
-    
--- README.md
+++
@@ -1,2 +0,0 @@
-# C5551_Studen-id3_ICP
-In Class programming class id -3
